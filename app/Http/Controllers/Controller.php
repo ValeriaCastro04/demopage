27b0b05af4639fe30915e5521795c50f3e1d1190
ValeriaCastro04 <180575811+ValeriaCastro04@users.noreply.github.com>
2025-02-06 17:51:17 -0600

    first commit

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+abstract class Controller
+{
+    // demopage.test/api/v1/email/preview
+    // eso puse en postman para visualizar las cosas
+    //y estos datos:
+    //{
+    //"customer": "John Doe",
+    //"email": "demo@test.com",
+    //"payment_method": 1,
+    //"products": [
+    //    {
+    //        "name": "product 1",
+    //        "price": 13.99,
+    //        "quantity": 5
+    //    },
+    //    {
+    //        "name": "product 2",
+    //        "price": 3.99,
+    //        "quantity": 3
+    //    },
+    //    {
+    //        "name": "product 3",
+    //        "price": 12.10,
+    //        "quantity": 3
+    //    }
+    //  ]
+    //}
+    //Lo subi a git
+}
